c8ce40199a9c8b5b5574c66f5e92fd3ab7d65871
Joseph Malicke <joseph@malicke.org>
2025-04-07 11:01:09 -0500

    Prettier, theme directory, ui utils

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    rules: {
+      '@next/next/no-img-element': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+]
 
 export default eslintConfig;
